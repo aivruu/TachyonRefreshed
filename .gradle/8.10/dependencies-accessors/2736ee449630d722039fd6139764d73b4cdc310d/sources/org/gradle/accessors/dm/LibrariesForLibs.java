f9e718089a7777bd2dac3b4ae9519eb1a51cf7db
Hugo Torres <noreply@redactado.es>
2024-12-24 11:56:35 +0100

    Regenerate wrapper

---
+++ .gradle/8.10/dependencies-accessors/2736ee449630d722039fd6139764d73b4cdc310d/sources/org/gradle/accessors/dm/LibrariesForLibs.java
@@ -0,0 +1,385 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.MinimalExternalModuleDependency;
+import org.gradle.plugin.use.PluginDependency;
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle;
+import org.gradle.api.artifacts.MutableVersionConstraint;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ProviderFactory;
+import org.gradle.api.internal.catalog.AbstractExternalDependencyFactory;
+import org.gradle.api.internal.catalog.DefaultVersionCatalog;
+import java.util.Map;
+import org.gradle.api.internal.attributes.ImmutableAttributesFactory;
+import org.gradle.api.internal.artifacts.dsl.CapabilityNotationParser;
+import javax.inject.Inject;
+
+/**
+ * A catalog of dependencies accessible via the {@code libs} extension.
+ */
+@NonNullApi
+public class LibrariesForLibs extends AbstractExternalDependencyFactory {
+
+    private final AbstractExternalDependencyFactory owner = this;
+    private final IoLibraryAccessors laccForIoLibraryAccessors = new IoLibraryAccessors(owner);
+    private final OrgLibraryAccessors laccForOrgLibraryAccessors = new OrgLibraryAccessors(owner);
+    private final VersionAccessors vaccForVersionAccessors = new VersionAccessors(providers, config);
+    private final BundleAccessors baccForBundleAccessors = new BundleAccessors(objects, providers, config, attributesFactory, capabilityNotationParser);
+    private final PluginAccessors paccForPluginAccessors = new PluginAccessors(providers, config);
+
+    @Inject
+    public LibrariesForLibs(DefaultVersionCatalog config, ProviderFactory providers, ObjectFactory objects, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) {
+        super(config, providers, objects, attributesFactory, capabilityNotationParser);
+    }
+
+    /**
+     * Group of libraries at <b>io</b>
+     */
+    public IoLibraryAccessors getIo() {
+        return laccForIoLibraryAccessors;
+    }
+
+    /**
+     * Group of libraries at <b>org</b>
+     */
+    public OrgLibraryAccessors getOrg() {
+        return laccForOrgLibraryAccessors;
+    }
+
+    /**
+     * Group of versions at <b>versions</b>
+     */
+    public VersionAccessors getVersions() {
+        return vaccForVersionAccessors;
+    }
+
+    /**
+     * Group of bundles at <b>bundles</b>
+     */
+    public BundleAccessors getBundles() {
+        return baccForBundleAccessors;
+    }
+
+    /**
+     * Group of plugins at <b>plugins</b>
+     */
+    public PluginAccessors getPlugins() {
+        return paccForPluginAccessors;
+    }
+
+    public static class IoLibraryAccessors extends SubDependencyFactory {
+        private final IoPapermcLibraryAccessors laccForIoPapermcLibraryAccessors = new IoPapermcLibraryAccessors(owner);
+
+        public IoLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Group of libraries at <b>io.papermc</b>
+         */
+        public IoPapermcLibraryAccessors getPapermc() {
+            return laccForIoPapermcLibraryAccessors;
+        }
+
+    }
+
+    public static class IoPapermcLibraryAccessors extends SubDependencyFactory {
+        private final IoPapermcPaperLibraryAccessors laccForIoPapermcPaperLibraryAccessors = new IoPapermcPaperLibraryAccessors(owner);
+
+        public IoPapermcLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Group of libraries at <b>io.papermc.paper</b>
+         */
+        public IoPapermcPaperLibraryAccessors getPaper() {
+            return laccForIoPapermcPaperLibraryAccessors;
+        }
+
+    }
+
+    public static class IoPapermcPaperLibraryAccessors extends SubDependencyFactory {
+
+        public IoPapermcPaperLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Dependency provider for <b>api</b> with <b>io.papermc.paper:paper-api</b> coordinates and
+         * with version reference <b>io.papermc.paper.api</b>
+         * <p>
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getApi() {
+            return create("io.papermc.paper.api");
+        }
+
+    }
+
+    public static class OrgLibraryAccessors extends SubDependencyFactory {
+        private final OrgJetbrainsLibraryAccessors laccForOrgJetbrainsLibraryAccessors = new OrgJetbrainsLibraryAccessors(owner);
+        private final OrgJunitLibraryAccessors laccForOrgJunitLibraryAccessors = new OrgJunitLibraryAccessors(owner);
+
+        public OrgLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Group of libraries at <b>org.jetbrains</b>
+         */
+        public OrgJetbrainsLibraryAccessors getJetbrains() {
+            return laccForOrgJetbrainsLibraryAccessors;
+        }
+
+        /**
+         * Group of libraries at <b>org.junit</b>
+         */
+        public OrgJunitLibraryAccessors getJunit() {
+            return laccForOrgJunitLibraryAccessors;
+        }
+
+    }
+
+    public static class OrgJetbrainsLibraryAccessors extends SubDependencyFactory {
+
+        public OrgJetbrainsLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Dependency provider for <b>annotations</b> with <b>org.jetbrains:annotations</b> coordinates and
+         * with version reference <b>org.jetbrains.annotations</b>
+         * <p>
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getAnnotations() {
+            return create("org.jetbrains.annotations");
+        }
+
+    }
+
+    public static class OrgJunitLibraryAccessors extends SubDependencyFactory {
+        private final OrgJunitJupiterLibraryAccessors laccForOrgJunitJupiterLibraryAccessors = new OrgJunitJupiterLibraryAccessors(owner);
+
+        public OrgJunitLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Group of libraries at <b>org.junit.jupiter</b>
+         */
+        public OrgJunitJupiterLibraryAccessors getJupiter() {
+            return laccForOrgJunitJupiterLibraryAccessors;
+        }
+
+    }
+
+    public static class OrgJunitJupiterLibraryAccessors extends SubDependencyFactory {
+        private final OrgJunitJupiterJunitLibraryAccessors laccForOrgJunitJupiterJunitLibraryAccessors = new OrgJunitJupiterJunitLibraryAccessors(owner);
+
+        public OrgJunitJupiterLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Group of libraries at <b>org.junit.jupiter.junit</b>
+         */
+        public OrgJunitJupiterJunitLibraryAccessors getJunit() {
+            return laccForOrgJunitJupiterJunitLibraryAccessors;
+        }
+
+    }
+
+    public static class OrgJunitJupiterJunitLibraryAccessors extends SubDependencyFactory {
+        private final OrgJunitJupiterJunitJupiterLibraryAccessors laccForOrgJunitJupiterJunitJupiterLibraryAccessors = new OrgJunitJupiterJunitJupiterLibraryAccessors(owner);
+
+        public OrgJunitJupiterJunitLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Group of libraries at <b>org.junit.jupiter.junit.jupiter</b>
+         */
+        public OrgJunitJupiterJunitJupiterLibraryAccessors getJupiter() {
+            return laccForOrgJunitJupiterJunitJupiterLibraryAccessors;
+        }
+
+    }
+
+    public static class OrgJunitJupiterJunitJupiterLibraryAccessors extends SubDependencyFactory {
+
+        public OrgJunitJupiterJunitJupiterLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Dependency provider for <b>engine</b> with <b>org.junit.jupiter:junit-jupiter-engine</b> coordinates and
+         * with version reference <b>org.junit.jupiter.junit.jupiter.engine</b>
+         * <p>
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getEngine() {
+            return create("org.junit.jupiter.junit.jupiter.engine");
+        }
+
+    }
+
+    public static class VersionAccessors extends VersionFactory  {
+
+        private final IoVersionAccessors vaccForIoVersionAccessors = new IoVersionAccessors(providers, config);
+        private final OrgVersionAccessors vaccForOrgVersionAccessors = new OrgVersionAccessors(providers, config);
+        public VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Group of versions at <b>versions.io</b>
+         */
+        public IoVersionAccessors getIo() {
+            return vaccForIoVersionAccessors;
+        }
+
+        /**
+         * Group of versions at <b>versions.org</b>
+         */
+        public OrgVersionAccessors getOrg() {
+            return vaccForOrgVersionAccessors;
+        }
+
+    }
+
+    public static class IoVersionAccessors extends VersionFactory  {
+
+        private final IoPapermcVersionAccessors vaccForIoPapermcVersionAccessors = new IoPapermcVersionAccessors(providers, config);
+        public IoVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Group of versions at <b>versions.io.papermc</b>
+         */
+        public IoPapermcVersionAccessors getPapermc() {
+            return vaccForIoPapermcVersionAccessors;
+        }
+
+    }
+
+    public static class IoPapermcVersionAccessors extends VersionFactory  {
+
+        private final IoPapermcPaperVersionAccessors vaccForIoPapermcPaperVersionAccessors = new IoPapermcPaperVersionAccessors(providers, config);
+        public IoPapermcVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Group of versions at <b>versions.io.papermc.paper</b>
+         */
+        public IoPapermcPaperVersionAccessors getPaper() {
+            return vaccForIoPapermcPaperVersionAccessors;
+        }
+
+    }
+
+    public static class IoPapermcPaperVersionAccessors extends VersionFactory  {
+
+        public IoPapermcPaperVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Version alias <b>io.papermc.paper.api</b> with value <b>1.21.4-R0.1-SNAPSHOT</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getApi() { return getVersion("io.papermc.paper.api"); }
+
+    }
+
+    public static class OrgVersionAccessors extends VersionFactory  {
+
+        private final OrgJetbrainsVersionAccessors vaccForOrgJetbrainsVersionAccessors = new OrgJetbrainsVersionAccessors(providers, config);
+        private final OrgJunitVersionAccessors vaccForOrgJunitVersionAccessors = new OrgJunitVersionAccessors(providers, config);
+        public OrgVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Group of versions at <b>versions.org.jetbrains</b>
+         */
+        public OrgJetbrainsVersionAccessors getJetbrains() {
+            return vaccForOrgJetbrainsVersionAccessors;
+        }
+
+        /**
+         * Group of versions at <b>versions.org.junit</b>
+         */
+        public OrgJunitVersionAccessors getJunit() {
+            return vaccForOrgJunitVersionAccessors;
+        }
+
+    }
+
+    public static class OrgJetbrainsVersionAccessors extends VersionFactory  {
+
+        public OrgJetbrainsVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Version alias <b>org.jetbrains.annotations</b> with value <b>24.1.0</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getAnnotations() { return getVersion("org.jetbrains.annotations"); }
+
+    }
+
+    public static class OrgJunitVersionAccessors extends VersionFactory  {
+
+        private final OrgJunitJupiterVersionAccessors vaccForOrgJunitJupiterVersionAccessors = new OrgJunitJupiterVersionAccessors(providers, config);
+        public OrgJunitVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Group of versions at <b>versions.org.junit.jupiter</b>
+         */
+        public OrgJunitJupiterVersionAccessors getJupiter() {
+            return vaccForOrgJunitJupiterVersionAccessors;
+        }
+
+    }
+
+    public static class OrgJunitJupiterVersionAccessors extends VersionFactory  {
+
+        private final OrgJunitJupiterJunitVersionAccessors vaccForOrgJunitJupiterJunitVersionAccessors = new OrgJunitJupiterJunitVersionAccessors(providers, config);
+        public OrgJunitJupiterVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Group of versions at <b>versions.org.junit.jupiter.junit</b>
+         */
+        public OrgJunitJupiterJunitVersionAccessors getJunit() {
+            return vaccForOrgJunitJupiterJunitVersionAccessors;
+        }
+
+    }
+
+    public static class OrgJunitJupiterJunitVersionAccessors extends VersionFactory  {
+
+        private final OrgJunitJupiterJunitJupiterVersionAccessors vaccForOrgJunitJupiterJunitJupiterVersionAccessors = new OrgJunitJupiterJunitJupiterVersionAccessors(providers, config);
+        public OrgJunitJupiterJunitVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Group of versions at <b>versions.org.junit.jupiter.junit.jupiter</b>
+         */
+        public OrgJunitJupiterJunitJupiterVersionAccessors getJupiter() {
+            return vaccForOrgJunitJupiterJunitJupiterVersionAccessors;
+        }
+
+    }
+
+    public static class OrgJunitJupiterJunitJupiterVersionAccessors extends VersionFactory  {
+
+        public OrgJunitJupiterJunitJupiterVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Version alias <b>org.junit.jupiter.junit.jupiter.engine</b> with value <b>5.10.2</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getEngine() { return getVersion("org.junit.jupiter.junit.jupiter.engine"); }
+
+    }
+
+    public static class BundleAccessors extends BundleFactory {
+
+        public BundleAccessors(ObjectFactory objects, ProviderFactory providers, DefaultVersionCatalog config, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) { super(objects, providers, config, attributesFactory, capabilityNotationParser); }
+
+    }
+
+    public static class PluginAccessors extends PluginFactory {
+
+        public PluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+    }
+
+}
