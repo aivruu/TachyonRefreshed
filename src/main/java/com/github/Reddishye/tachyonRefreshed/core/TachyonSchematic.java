e3a8fec994b22a81ad5302c01f3e598d656e1d12
aivruu <benjaalejandro206@gmail.com>
2025-01-21 21:57:55 -0300

    chore(TachyonSchematic): change code-indentation and change values to assign when 180-flip

--- src/main/java/com/github/Reddishye/tachyonRefreshed/core/TachyonSchematic.java
+++ src/main/java/com/github/Reddishye/tachyonRefreshed/core/TachyonSchematic.java
@@ -23,267 +23,264 @@ import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 public class TachyonSchematic implements Schematic {
-    private static final Logger LOGGER = Logger.getLogger("TachyonRefreshed");
+  private static final Logger LOGGER = Logger.getLogger("TachyonRefreshed");
 
-    private record RelativeBlock(int x, int y, int z, BlockData blockData) implements Serializable {
-        public Location toAbsolute(Location origin) {
-            return new Location(
-                    origin.getWorld(), origin.getBlockX(), origin.getBlockY(), origin.getBlockZ(),
-                    origin.getYaw(), origin.getPitch()
-            ).add(x, y, z);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof RelativeBlock that)) return false;
-            return x == that.x && y == that.y && z == that.z;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y, z);
-        }
-    }
-
-//    private final Set<RelativeBlock> blocks = ConcurrentHashMap.newKeySet();
-    private final BlockChangePacketSender packetSender;
-    private final int width;
-    private final int height;
-    private final int length;
-    // Experimental, use array to reduce object-allocation.
-    private RelativeBlock[] blocks;
-
-    @AssistedInject
-    public TachyonSchematic(
-            @Assisted("start") Location start,
-            @Assisted("end") Location end,
-            @Assisted("origin") Location origin,
-            BlockChangePacketSender packetSender) {
-        this.packetSender = packetSender;
-        this.width = Math.abs(end.getBlockX() - start.getBlockX()) + 1;
-        this.height = Math.abs(end.getBlockY() - start.getBlockY()) + 1;
-        this.length = Math.abs(end.getBlockZ() - start.getBlockZ()) + 1;
-        this.copyBlocks(start, end, origin);
-    }
-
-    private void copyBlocks(Location start, Location end, Location origin) {
-        int maxX = Math.max(start.getBlockX(), end.getBlockX());
-        int maxY = Math.max(start.getBlockY(), end.getBlockY());
-        int maxZ = Math.max(start.getBlockZ(), end.getBlockZ());
-
-        List<Vector> coordinates = new ArrayList<>();
-        for (int x = Math.min(start.getBlockX(), end.getBlockX()); x <= maxX; x++) {
-            for (int y = Math.min(start.getBlockY(), end.getBlockY()); y <= maxY; y++) {
-                for (int z = Math.min(start.getBlockZ(), end.getBlockZ()); z <= maxZ; z++) {
-                    coordinates.add(new Vector(x, y, z));
-                }
-            }
-        }
-        this.blocks = new RelativeBlock[coordinates.size()];
-        Vector vec;
-        for (int i = 0; i < this.blocks.length; i++) {
-            vec = coordinates.get(i);
-            int vecX = vec.getBlockX();
-            int vecY = vec.getBlockY();
-            int vecZ = vec.getBlockZ();
-            this.blocks[i] = new RelativeBlock(
-                    vecX - origin.getBlockX(),
-                    vecY - origin.getBlockY(),
-                    vecZ - origin.getBlockZ(),
-                    start.getWorld().getBlockData(vecX, vecY, vecZ));
-        }
+  private record RelativeBlock(int x, int y, int z, BlockData blockData) implements Serializable {
+    public Location toAbsolute(Location origin) {
+      return new Location(
+        origin.getWorld(), origin.getBlockX(), origin.getBlockY(), origin.getBlockZ(),
+        origin.getYaw(), origin.getPitch()
+      ).add(x, y, z);
     }
 
     @Override
-    public CompletableFuture<Void> pasteAsync(Location pasteLocation, boolean ignoreAir) {
-        return CompletableFuture.runAsync(() -> this.notifyBlockChanges(pasteLocation, ignoreAir, true));
-    }
-
-    private void notifyBlockChanges(final Location pasteLocation, final boolean ignoreAir, final boolean async) {
-        final List<Location> locations = new ArrayList<>();
-        final Map<Location, BlockData> blockChanges = new HashMap<>();
-        if (async) {
-            synchronized (this.blocks) {
-                this.prepareBlocksForUpdate(locations, blockChanges, pasteLocation, ignoreAir);
-            }
-        } else {
-            this.prepareBlocksForUpdate(locations, blockChanges, pasteLocation, ignoreAir);
-        }
-        packetSender.sendBlockChanges(locations, blockChanges);
-    }
-
-    private void prepareBlocksForUpdate(
-            final List<Location> locations, final Map<Location, BlockData> blockChanges,
-            final Location pasteLocation, final boolean ignoreAir
-    ) {
-        Location location;
-        for (final RelativeBlock block : this.blocks) {
-            if (ignoreAir && block.blockData.getMaterial() == Material.AIR) {
-                continue;
-            }
-            location = block.toAbsolute(pasteLocation);
-            locations.add(location);
-            blockChanges.put(location, block.blockData);
-        }
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof RelativeBlock that)) return false;
+      return x == that.x && y == that.y && z == that.z;
     }
 
     @Override
-    public void pasteSync(Location pasteLocation, boolean ignoreAir) {
-        this.notifyBlockChanges(pasteLocation, ignoreAir, false);
+    public int hashCode() {
+      return Objects.hash(x, y, z);
     }
+  }
 
-    @Override
-    public CompletableFuture<Void> saveAsync(File file) {
-        return CompletableFuture.runAsync(() -> {
-            try (DataOutputStream out = new DataOutputStream(
-                    new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(file))))) {
-                out.writeInt(width);
-                out.writeInt(height);
-                out.writeInt(length);
-                out.writeInt(this.blocks.length);
+  //    private final Set<RelativeBlock> blocks = ConcurrentHashMap.newKeySet();
+  private final BlockChangePacketSender packetSender;
+  private final int width;
+  private final int height;
+  private final int length;
+  // Experimental, use array to reduce object-allocation.
+  private RelativeBlock[] blocks;
 
-                synchronized (this.blocks) {
-                    for (final RelativeBlock block : this.blocks) {
-                        out.writeInt(block.x);
-                        out.writeInt(block.y);
-                        out.writeInt(block.z);
-                        out.writeUTF(block.blockData.getAsString());
-                    }
-                }
-            } catch (final IOException exception) {
-                LOGGER.severe("Failed to load schematic, check by corrupt-data or file syntax-invalid.");
-            }
-        });
-    }
+  @AssistedInject
+  public TachyonSchematic(
+    @Assisted("start") Location start,
+    @Assisted("end") Location end,
+    @Assisted("origin") Location origin,
+    BlockChangePacketSender packetSender) {
+    this.packetSender = packetSender;
+    this.width = Math.abs(end.getBlockX() - start.getBlockX()) + 1;
+    this.height = Math.abs(end.getBlockY() - start.getBlockY()) + 1;
+    this.length = Math.abs(end.getBlockZ() - start.getBlockZ()) + 1;
+    this.copyBlocks(start, end, origin);
+  }
 
-    public static CompletableFuture<@Nullable Schematic> loadAsync(File file, BlockChangePacketSender packetSender) {
-        return CompletableFuture.supplyAsync(() -> {
-            try (DataInputStream in = new DataInputStream(
-                    new BufferedInputStream(new GZIPInputStream(new FileInputStream(file))))) {
+  private void copyBlocks(Location start, Location end, Location origin) {
+    int maxX = Math.max(start.getBlockX(), end.getBlockX());
+    int maxY = Math.max(start.getBlockY(), end.getBlockY());
+    int maxZ = Math.max(start.getBlockZ(), end.getBlockZ());
 
-                int width = in.readInt();
-                int height = in.readInt();
-                int length = in.readInt();
-                int blockCount = in.readInt();
-
-                final RelativeBlock[] blocks = new RelativeBlock[blockCount];
-                for (int i = 0; i < blockCount; i++) {
-                    blocks[i] = new RelativeBlock(
-                            in.readInt(),
-                            in.readInt(),
-                            in.readInt(),
-                            Bukkit.createBlockData(in.readUTF()));
-                }
-
-                return new TachyonSchematic(blocks, width, height, length, packetSender);
-            } catch (final IOException exception) {
-                LOGGER.severe("Failed to load schematic, check by corrupt-data or file syntax-invalid.");
-                return null;
-            }
-        });
-    }
-
-    private TachyonSchematic(
-            RelativeBlock[] blocks,
-            int width,
-            int height,
-            int length,
-            BlockChangePacketSender packetSender) {
-        this.blocks = blocks;
-        this.width = width;
-        this.height = height;
-        this.length = length;
-        this.packetSender = packetSender;
-    }
-
-    @Override
-    public void rotate(double angle) {
-        angle = angle % 360;
-        if (angle < 0) angle += 360;
-
-        final StructureRotation rotation = switch ((int) angle) {
-            case 90 -> StructureRotation.CLOCKWISE_90;
-            case 180 -> StructureRotation.CLOCKWISE_180;
-            case 270 -> StructureRotation.COUNTERCLOCKWISE_90;
-            default -> null;
-        };
-        if (rotation == null) {
-            return;
+    List<Vector> coordinates = new ArrayList<>();
+    for (int x = Math.min(start.getBlockX(), end.getBlockX()); x <= maxX; x++) {
+      for (int y = Math.min(start.getBlockY(), end.getBlockY()); y <= maxY; y++) {
+        for (int z = Math.min(start.getBlockZ(), end.getBlockZ()); z <= maxZ; z++) {
+          coordinates.add(new Vector(x, y, z));
         }
-        BlockData rotatedData;
-        RelativeBlock block;
-        for (int i = 0; i < this.blocks.length; i++) {
-            int newX;
-            int newZ;
-            block = this.blocks[i];
-            switch (rotation) {
-                case CLOCKWISE_90 -> {
-                    newX = -block.z;
-                    newZ = block.x;
-                }
-                case CLOCKWISE_180 -> {
-                    newX = -block.z;
-                    newZ = -block.x;
-                }
-                case COUNTERCLOCKWISE_90 -> {
-                    newX = block.z;
-                    newZ = -block.x;
-                }
-                // 'NONE' enum-constant reaching.
-                default -> {
-                    newX = block.z;
-                    newZ = block.x;
-                }
-            }
-            rotatedData = block.blockData.clone();
-            rotatedData.rotate(rotation);
-            this.blocks[i] = new RelativeBlock(newX, block.y, newZ, rotatedData);
-        }
+      }
     }
+    this.blocks = new RelativeBlock[coordinates.size()];
+    Vector vec;
+    for (int i = 0; i < this.blocks.length; i++) {
+      vec = coordinates.get(i);
+      int vecX = vec.getBlockX();
+      int vecY = vec.getBlockY();
+      int vecZ = vec.getBlockZ();
+      this.blocks[i] = new RelativeBlock(
+        vecX - origin.getBlockX(),
+        vecY - origin.getBlockY(),
+        vecZ - origin.getBlockZ(),
+        start.getWorld().getBlockData(vecX, vecY, vecZ));
+    }
+  }
 
-    @Override
-    @Deprecated
-    public void flip(@NotNull String direction) {
-        FlipDirection flipDirection;
-        try {
-            flipDirection = FlipDirection.valueOf(direction.toUpperCase(Locale.ROOT));
-        } catch (final IllegalArgumentException exception) {
-            flipDirection = null;
-        }
-        if (flipDirection == null) {
-            LOGGER.severe("Invalid flip-direction value provided for conversion: " + direction);
-            return;
-        }
-        this.flip(flipDirection);
-    }
+  @Override
+  public CompletableFuture<Void> pasteAsync(Location pasteLocation, boolean ignoreAir) {
+    return CompletableFuture.runAsync(() -> this.notifyBlockChanges(pasteLocation, ignoreAir, true));
+  }
 
-    @Override
-    public void flip(@NotNull FlipDirection direction) {
-        final Mirror mirror = switch (direction) {
-            case UP, DOWN, LEFT, RIGHT -> Mirror.LEFT_RIGHT;
-            case NORTH, SOUTH -> Mirror.FRONT_BACK;
-        };
-        BlockData flippedData;
-        RelativeBlock block;
-        for (int i = 0; i < this.blocks.length; i++) {
-            block = this.blocks[i];
-            int newX = block.x;
-            int newY = block.y;
-            int newZ = block.z;
-            switch (direction) {
-                case LEFT, RIGHT -> newX = -block.x;
-                case UP, DOWN -> newY = -block.y;
-                case NORTH, SOUTH -> newZ = -block.z;
-            }
-            flippedData = block.blockData.clone();
-            flippedData.mirror(mirror);
-            this.blocks[i] = new RelativeBlock(newX, newY, newZ, flippedData);
-        }
+  private void notifyBlockChanges(final Location pasteLocation, final boolean ignoreAir, final boolean async) {
+    final List<Location> locations = new ArrayList<>();
+    final Map<Location, BlockData> blockChanges = new HashMap<>();
+    if (async) {
+      synchronized (this.blocks) {
+        this.prepareBlocksForUpdate(locations, blockChanges, pasteLocation, ignoreAir);
+      }
+    } else {
+      this.prepareBlocksForUpdate(locations, blockChanges, pasteLocation, ignoreAir);
     }
+    packetSender.sendBlockChanges(locations, blockChanges);
+  }
 
-    @Override
-    public int getBlockCount() {
-        return this.blocks.length;
+  private void prepareBlocksForUpdate(
+    final List<Location> locations, final Map<Location, BlockData> blockChanges,
+    final Location pasteLocation, final boolean ignoreAir
+  ) {
+    Location location;
+    for (final RelativeBlock block : this.blocks) {
+      if (ignoreAir && block.blockData.getMaterial() == Material.AIR) {
+        continue;
+      }
+      location = block.toAbsolute(pasteLocation);
+      locations.add(location);
+      blockChanges.put(location, block.blockData);
     }
+  }
+
+  @Override
+  public void pasteSync(Location pasteLocation, boolean ignoreAir) {
+    this.notifyBlockChanges(pasteLocation, ignoreAir, false);
+  }
+
+  @Override
+  public CompletableFuture<Void> saveAsync(File file) {
+    return CompletableFuture.runAsync(() -> {
+      try (DataOutputStream out = new DataOutputStream(
+        new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(file))))) {
+        out.writeInt(width);
+        out.writeInt(height);
+        out.writeInt(length);
+        out.writeInt(this.blocks.length);
+
+        synchronized (this.blocks) {
+          for (final RelativeBlock block : this.blocks) {
+            out.writeInt(block.x);
+            out.writeInt(block.y);
+            out.writeInt(block.z);
+            out.writeUTF(block.blockData.getAsString());
+          }
+        }
+      } catch (final IOException exception) {
+        LOGGER.severe("Failed to load schematic, check by corrupt-data or file syntax-invalid.");
+      }
+    });
+  }
+
+  public static CompletableFuture<@Nullable Schematic> loadAsync(File file, BlockChangePacketSender packetSender) {
+    return CompletableFuture.supplyAsync(() -> {
+      try (DataInputStream in = new DataInputStream(
+        new BufferedInputStream(new GZIPInputStream(new FileInputStream(file))))) {
+
+        int width = in.readInt();
+        int height = in.readInt();
+        int length = in.readInt();
+        int blockCount = in.readInt();
+
+        final RelativeBlock[] blocks = new RelativeBlock[blockCount];
+        for (int i = 0; i < blockCount; i++) {
+          blocks[i] = new RelativeBlock(
+            in.readInt(),
+            in.readInt(),
+            in.readInt(),
+            Bukkit.createBlockData(in.readUTF()));
+        }
+
+        return new TachyonSchematic(blocks, width, height, length, packetSender);
+      } catch (final IOException exception) {
+        LOGGER.severe("Failed to load schematic, check by corrupt-data or file syntax-invalid.");
+        return null;
+      }
+    });
+  }
+
+  private TachyonSchematic(
+    RelativeBlock[] blocks,
+    int width,
+    int height,
+    int length,
+    BlockChangePacketSender packetSender) {
+    this.blocks = blocks;
+    this.width = width;
+    this.height = height;
+    this.length = length;
+    this.packetSender = packetSender;
+  }
+
+  @Override
+  public void rotate(double angle) {
+    angle = angle % 360;
+    if (angle < 0) {
+      angle += 360;
+    }
+    final StructureRotation rotation = switch ((int) angle) {
+      case 90 -> StructureRotation.CLOCKWISE_90;
+      case 180 -> StructureRotation.CLOCKWISE_180;
+      case 270 -> StructureRotation.COUNTERCLOCKWISE_90;
+      default -> null;
+    };
+    if (rotation == null) {
+      return;
+    }
+    BlockData rotatedData;
+    RelativeBlock block;
+    for (int i = 0; i < this.blocks.length; i++) {
+      block = this.blocks[i];
+      int newX = block.x;
+      int newZ = block.z;
+      switch (rotation) {
+        case CLOCKWISE_90 -> {
+          newX = -block.z;
+          newZ = block.x;
+        }
+        case CLOCKWISE_180 -> {
+          newX = -block.x;
+          newZ = -block.z;
+        }
+        case COUNTERCLOCKWISE_90 -> {
+          newX = block.z;
+          newZ = -block.x;
+        }
+      }
+      rotatedData = block.blockData.clone();
+      rotatedData.rotate(rotation);
+      this.blocks[i] = new RelativeBlock(newX, block.y, newZ, rotatedData);
+    }
+  }
+
+  @Override
+  @Deprecated
+  public void flip(@NotNull String direction) {
+    FlipDirection flipDirection;
+    try {
+      flipDirection = FlipDirection.valueOf(direction.toUpperCase(Locale.ROOT));
+    } catch (final IllegalArgumentException exception) {
+      LOGGER.warning("Exception occurred during flip-direction value provided validating.");
+      flipDirection = null;
+    }
+    if (flipDirection == null) {
+      LOGGER.severe("Invalid flip-direction value provided for conversion: " + direction);
+      return;
+    }
+    this.flip(flipDirection);
+  }
+
+  @Override
+  public void flip(@NotNull FlipDirection direction) {
+    final Mirror mirror = switch (direction) {
+      case UP, DOWN, LEFT, RIGHT -> Mirror.LEFT_RIGHT;
+      case NORTH, SOUTH -> Mirror.FRONT_BACK;
+    };
+    BlockData flippedData;
+    RelativeBlock block;
+    for (int i = 0; i < this.blocks.length; i++) {
+      block = this.blocks[i];
+      int newX = block.x;
+      int newY = block.y;
+      int newZ = block.z;
+      switch (direction) {
+        case LEFT, RIGHT -> newX = -block.x;
+        case UP, DOWN -> newY = -block.y;
+        case NORTH, SOUTH -> newZ = -block.z;
+      }
+      flippedData = block.blockData.clone();
+      flippedData.mirror(mirror);
+      this.blocks[i] = new RelativeBlock(newX, newY, newZ, flippedData);
+    }
+  }
+
+  @Override
+  public int getBlockCount() {
+    return this.blocks.length;
+  }
 }
